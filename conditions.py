8faae9ffd51ca1ff8251742c08899d752ac460f0
Vidyansh07 <Vidyansht@gmail.com>
2024-04-27 15:25:26 +0530

    Basics of Data Science

---
+++ conditions.py
@@ -0,0 +1,18 @@
+def conditions():
+    x = 1
+    if x == 1:
+        print('x is 1')
+    elif x == 2:
+        print('x is 2')
+    else:
+        print('x is not 1 or 2')
+    return
+
+def nested_conditions():
+
+    for i in range(1,7):
+        for j in range(1,7):
+            print(i,j)
+            break     
+        print(p      
+nested_conditions()
